cc30838a3e1a0fd77e3f5bedeb324804e2b06dad
Quentin Rodriguez <rodriguez.quentin@protonmail.com>
2025-10-04 14:43:16 +0200

    Move filesystem logic in utils directory

---
+++ lua/utils/fs.lua
@@ -0,0 +1,38 @@
+local M = {}
+local fn = vim.fn
+local uv = vim.uv or vim.loop
+
+function M.join(...)
+  return vim.fs.normalize(table.concat({...}, "/"))
+end
+
+function M.glob(expr)
+  return vim.split(fn.glob(expr), "\n", { trimempty = true })
+end
+
+function M.is_file(path)
+  local stat = uv.fs_stat(path)
+  return stat and stat.type == "file"
+end
+
+function M.is_directory(path)
+  local stat = uv.fs_stat(path)
+  return stat and stat.type == "directory"
+end
+
+function M.mkdir(path)
+  if not M.is_directory(path) then
+    fn.mkdir(path, "p")
+  end
+end
+
+function M.read(path)
+  return fn.readblob(path)
+end
+
+function M.write(path, content)
+  fn.writefile({ content }, path, "b")
+end
+
+return M
+
